ab092c9b7d540b24683adfb716e3717a6bd5d148
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-08-13 09:55:19 -0400

    initial xcode commit

---
+++ Woke/Woke/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Woke
+//
+//  Created by Ahmed Fuad Ali on 2018-08-13.
+//  Copyright © 2018 Ahmed Fuad Ali. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
